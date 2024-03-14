89d5ca361abc02013ddbb284656a61071c914200
Ida Whitcomb <idawhitcomb@gmail.com>
2024-03-14 15:26:34 -0400

    Began fixing the save book bug

--- Develop GraphQL API/server/server.js
+++ Develop GraphQL API/server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
 const startApolloServer = async () => {
